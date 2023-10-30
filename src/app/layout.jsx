6690a8db8a34bffd389c5caa3ea6efbcf469504e
aynaz <aynaz@arianalabs.io>
2023-10-30 10:25:37 +0330

    FEAT : add base folders

---
+++ src/app/layout.jsx
@@ -0,0 +1,17 @@
+// import Footer from "@/components/footer";
+import "./globals.css";
+// import Head from "./head";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/* <Head /> */}
+      <body className="relative min-h-screen" suppressHydrationWarning={true}>
+        {children}
+
+        {/* footer */}
+        {/* <Footer /> */}
+      </body>
+    </html>
+  );
+}
